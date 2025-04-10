67a5e2bf14ceb2135ab3df5c3478e86e0cc4f297
jay patel <jpatel8384@conestoga.ca.on>
2025-04-10 00:42:24 -0400

    Implement AJAX call to fetch Post objects from server.

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,7 +1,7 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world');
-
+const postsBox = document.getElementById('posts-box');
 
 
 $.ajax({
@@ -15,3 +15,23 @@ $.ajax({
         console.log('error', error);
     }
 });
+
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br><br>
+            `
+        });
+    },
+    error: function(error) {
+        console.log(error);
+    }
+});
+
