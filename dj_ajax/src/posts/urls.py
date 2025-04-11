e4bf0a1259ab8292a74a606b5f7fff4ca5e7f4b7
jay patel <jpatel8384@conestoga.ca.on>
2025-04-11 18:41:35 -0400

    completed the post detail page and added the modals of update and delete post

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,8 +3,8 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    post_detail
-
+    post_detail,
+    post_detail_data_view
 )
 
 app_name = 'posts'
@@ -13,6 +13,7 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     
 ]
